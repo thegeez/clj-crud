7b15294ac6bd4273d3e1d4080646e31acf44fb32
thegeez <thegeez@users.noreply.github.com>
2014-03-31 21:40:46 +0200

    Another db url

--- src/clj_crud/main.clj
+++ src/clj_crud/main.clj
@@ -1,7 +1,9 @@
 (ns clj-crud.main
   (:require [clojure.tools.logging :refer [info debug]]
             [com.stuartsierra.component :as component]
-            [clj-crud.core :as core])
+            [clj-crud.core :as core]
+            [clojure.string :as string])
+  (:import [java.net.URI])
   (:gen-class))
 
 (defn -main [& args]
@@ -11,7 +13,12 @@
         database-url (let [db-url (second args)]
                        (assert (.startsWith db-url "postgres:")
                                (str "Something is wrong with the database argument: " (second args)))
-                       db-url)
+                       (let [db-uri (URI. db-url)
+                             host (.getHost db-uri)
+                             port (.getPort db-uri)
+                             path (.getPath db-uri)
+                             [user password] (string/split (.getUserInfo db-uri) #":")]
+                         (str "jdbc:postgresql://" host ":" port path "?user=" user "&password=" password "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")))
         system (core/crud-system (core/production-config port database-url))]
     (component/start system)
     (.addShutdownHook (Runtime/getRuntime)
